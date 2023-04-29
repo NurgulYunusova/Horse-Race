7ba2365d7b30b90f43a32c32d1281f75387fc4e0
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2023-04-29 23:56:23 +0400

    Added some parts

--- script.js
+++ script.js
@@ -3,12 +3,34 @@ const secondHorse = document.querySelector(".horse2");
 const thirdHorse = document.querySelector(".horse3");
 const startButton = document.querySelector(".startBtn");
 const horseAudio = document.querySelector(".horseAudioMp3");
+const horseList = document.querySelector(".horseList");
+const amount = document.querySelector(".amount");
+const depositButton = document.querySelector(".depositBtn");
+const price = document.querySelector(".price");
+
+let amountOfDeposit;
+let chosenHorse = "";
+let wallet = 0;
+
+depositButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  chosenHorse = horseList.value;
+  console.log(chosenHorse);
+
+  amountOfDeposit = amount.value;
+  console.log(amountOfDeposit);
+
+  amount.value = 0;
+  horseList.options.selectedIndex = 0;
+});
 
 startButton.addEventListener("click", () => {
   // setInterval(function () {
   //   horseAudio.play();
   // }, 1100);
 
+  horseAudio.currentTime = 0;
   horseAudio.play();
 
   let firstHorsePlace = 0;
@@ -16,13 +38,13 @@ startButton.addEventListener("click", () => {
   let thirdHorsePlace = 0;
 
   let race = setInterval(function () {
-    firstHorsePlace += Math.floor(Math.random() * 30);
+    firstHorsePlace += Math.floor(Math.random() * 20);
     firstHorse.style.left = `${firstHorsePlace}px`;
 
-    secondHorsePlace += Math.floor(Math.random() * 30);
+    secondHorsePlace += Math.floor(Math.random() * 20);
     secondHorse.style.left = `${secondHorsePlace}px`;
 
-    thirdHorsePlace += Math.floor(Math.random() * 30);
+    thirdHorsePlace += Math.floor(Math.random() * 20);
     thirdHorse.style.left = `${thirdHorsePlace}px`;
     console.log(firstHorsePlace, secondHorsePlace, thirdHorsePlace);
 
@@ -32,9 +54,11 @@ startButton.addEventListener("click", () => {
       thirdHorsePlace >= 600
     ) {
       clearInterval(race);
+      horseAudio.pause();
 
       if (firstHorsePlace >= 600) {
         alert("First horse win!");
+        price.innerText = amountOfDeposit;
 
         firstHorsePlace = 0;
         secondHorsePlace = 0;
@@ -65,5 +89,5 @@ startButton.addEventListener("click", () => {
         thirdHorse.style.left = "0px";
       }
     }
-  }, 300);
+  }, 150);
 });
